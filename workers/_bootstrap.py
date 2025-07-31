4c8d759e827646be5d297c8a90ad8c7622a0c4ec
ageorge95 <arteni.george.daniel@gmail.com>
2025-07-31 19:40:11 +0300

    removed the complex configuration opening logic from WorkerBootstrap

--- workers/_bootstrap.py
+++ workers/_bootstrap.py
@@ -7,15 +7,8 @@ class WorkerBootstrap:
         self.working = False
 
         # load the configuration json file
-        self.config = {}
-        possible_config_filepaths = ['configuration.json',
-                                     os.path.join('.', 'configuration.json')]
-        for config_filepath in possible_config_filepaths:
-            if os.path.isfile(config_filepath):
-                with open('configuration.json', 'r') as f:
-                    self.config = json.load(f)
-        if not self.config:
-            raise Exception('Configuration file not found.')
+        with open('configuration.json', 'r') as f:
+            self.config = json.load(f)
 
     def is_working(self):
         return self.working
