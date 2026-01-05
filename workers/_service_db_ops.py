1810414c0f620a44509534bd853357ba10b1b22e
ageorge95 <arteni.george.daniel@gmail.com>
2026-01-05 13:56:40 +0200

    used a custom exit mechanism in _service_db_ops.py

--- workers/_service_db_ops.py
+++ workers/_service_db_ops.py
@@ -1,10 +1,19 @@
 import os
+import time
+import threading
 from ._loader import register_worker
 from ._bootstrap import WorkerBootstrap
 from logging import getLogger
 from ag95 import (configure_logger,
                   initialize_SqliteDbWrapper_service)
 from traceback import format_exc
+
+def exit_file_watcher(log, exit_flag_func):
+    while True:
+        if exit_flag_func():
+            log.info(f"Exit command detected. Sending os._exit ...")
+            os._exit(0)
+        time.sleep(1)
 
 @register_worker(worker_cycle_time_s=0,
                  worker_name=os.path.basename(__file__).replace('.py', ''))
@@ -28,11 +37,19 @@ class Worker(WorkerBootstrap):
         try:
             self._log.info('Starting service ...')
 
+            # start exit file watcher
+            watcher = threading.Thread(
+                target=exit_file_watcher,
+                args=(self._log, self.should_exit),
+                daemon=True
+            )
+            watcher.start()
+
             initialize_SqliteDbWrapper_service(LOCALHOST_ONLY=True,
                                                SERVICE_PORT=self.config.get(reload=True)['db_ops_port'],
                                                database_path=os.path.join('db', 'database', 'database.sqlite'),
                                                timeout=5*60,
-                                               shutdown_watcher_mode="wait_for_exit_file")
+                                               shutdown_watcher_mode="none")
 
             # the only way to gracefully close the service is with a SIGTERM, so the code should never reach this code
             # but, it is still here for consistency
